906b9c7a01397cc5f5996fe8ab10d89f5dd354ca
yuvaraj540 <yuvanyuvarajan492@gmail.com>
2024-12-27 20:19:07 +0530

    Edit Profile Api

--- Chatappapi/Repository/ProfileRepository.cs
+++ Chatappapi/Repository/ProfileRepository.cs
@@ -18,15 +18,47 @@ public class ProfileRepository : IProfileRepository
 
     public async Task<Users?> GetProfileData(Guid model)
     {
-        var databaseconnection = _connectionFactory.OpenSqlConnection();
+        var databaseConnection = _connectionFactory.OpenSqlConnection();
 
         DynamicParameters parameters = new DynamicParameters();
 
         parameters.Add("id", model);
 
-        var result = await databaseconnection.QueryFirstOrDefaultAsync<Users>("GetProfileData", parameters,commandType: CommandType.StoredProcedure);
+        var result = await databaseConnection.QueryFirstOrDefaultAsync<Users>("GetProfileData", parameters,commandType: CommandType.StoredProcedure);
 
         return result;
     }
+
+    public async Task<int?> UpdateProfileData(updateProfile model)
+    {
+        try
+        {
+            
+            var databaseConnection = _connectionFactory.OpenSqlConnection();
+
+            
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("id", model.Id);
+            parameters.Add("name", model.Name);
+            parameters.Add("email", model.Email);
+            parameters.Add("phone", model.Phone);
+            parameters.Add("profileUrl", model.ProfileUrl);
+
+            
+            var result = await databaseConnection.QueryFirstOrDefaultAsync<int?>(
+                "UpdateProfileData",
+                parameters,
+                commandType: CommandType.StoredProcedure
+            );
+
+           
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("An error occurred while updating the profile in database.",ex);
+        }
+    }
+
 
 }
