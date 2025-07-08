3d9f788118679351001ab7520c516c08cf895d6c
saran-raj-p <ssaranraj803@gmail.com>
2025-07-08 16:22:56 +0530

    changed url

--- Chatappapi/obj/Debug/net8.0/Chatappapi.AssemblyInfo.cs
+++ Chatappapi/obj/Debug/net8.0/Chatappapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chatappapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc6df42293250de4ec8617dc1fcfb63e2e5ef5ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9f87207e0fcc6986d4a892820bf99c19fca518")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chatappapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chatappapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
